d57c4354c990f426a07d2b1e0a8f2bdad937b195
Trần Nhật Hào <trannhathao124@gmail.com>
2023-12-16 14:30:13 +0700

    small fix

--- src/components/modal_layout/ModalLayout.jsx
+++ src/components/modal_layout/ModalLayout.jsx
@@ -2,7 +2,7 @@ import CloseIcon from '@mui/icons-material/Close';
 
 const ModalLayout = ({ children, handleClose, title }) => {
     return (
-        <div className="fixed top-0 right-0 left-0 bottom-0 flex justify-center items-center content-center bg-gray-400/50 z-10">
+        <div className="fixed top-0 right-0 left-0 bottom-0 flex justify-center items-center content-center bg-gray-400/50 z-50">
             <div className="bg-white p-8 rounded shadow-md relative min-w-[400px]">
                 <button className="absolute top-2 right-2 text-gray-600 hover:text-gray-800 focus:outline-none" onClick={() => handleClose()}>
                     <CloseIcon></CloseIcon>
